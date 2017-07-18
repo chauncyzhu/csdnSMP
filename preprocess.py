0545b805aa6d17d25e7b4e43317049fcd7a91fcf
chauncyzhu <chauncyzhu@163.com>
2017-07-18 16:22:32 +0800

    获取训练以及测试数据

--- preprocess.py
+++ preprocess.py
@@ -10,6 +10,7 @@ def read_txt(filename,columns_name,target_file=None):
     :param filename:
     :return:
     """
+
     dic = {}
     lines = open(filename,encoding="utf8").readlines()
     for line in lines:
@@ -72,8 +73,7 @@ def read_pandas(filename):
     return pd_data
 
 
-
-def train_data(train_file=None):
+def train_and_dev_data(train_file=None,dev_file=None):
     pd_post = read_txt(dp.PostTxt,['uid','post'],dp.PostCSV)
     pd_brows = read_txt(dp.BrowseTxt,['uid','brows'],dp.BrowseCSV)
     pd_comment = read_txt(dp.CommentTxt,['uid','comment'],dp.CommentCSV)
@@ -89,6 +89,10 @@ def train_data(train_file=None):
 
     pd_train = read_txt(dp.TrainTxt,['uid','labels'],dp.TrainCsv)
 
+    with open(dp.DevTxt,encoding="utf8") as f:
+        pd_dev = pd.DataFrame({"uid": [line.strip() for line in f]})
+
+
     pd_train['post'] = [None]*len(pd_train)
     pd_train['brows'] = [None]*len(pd_train)
     pd_train['comment'] = [None]*len(pd_train)
@@ -97,19 +101,30 @@ def train_data(train_file=None):
     pd_train['favorite'] = [None]*len(pd_train)
     pd_train['follow'] = [None]*len(pd_train)
     pd_train['letter'] = [None]*len(pd_train)
-    pd_train['blog_uid'] = [[]]*len(pd_train)
+    pd_train['blog_id'] = [[]]*len(pd_train)
+
+    pd_dev['post'] = [None] * len(pd_dev)
+    pd_dev['brows'] = [None] * len(pd_dev)
+    pd_dev['comment'] = [None] * len(pd_dev)
+    pd_dev['voteup'] = [None] * len(pd_dev)
+    pd_dev['votedown'] = [None] * len(pd_dev)
+    pd_dev['favorite'] = [None] * len(pd_dev)
+    pd_dev['follow'] = [None] * len(pd_dev)
+    pd_dev['letter'] = [None] * len(pd_dev)
+    pd_dev['blog_id'] = [[]] * len(pd_dev)
 
     def f(pd_data,pd_train,index,row,con_column,target_column):
         if row[con_column] in list(pd_data[con_column]):  #如果pd中包含该uid，则找出对一个的value值，同时这个uid都是唯一的
             #添加blog的uid
             temp_list = list(pd_data[pd_data[con_column] == row[con_column]][target_column])[0]
             #print("blog info"+str(pd_train['blog_uid'][index]))
-            pd_train['blog_uid'][index].extend([i.split('\001')[0] for i in temp_list])
-            pd_train['blog_uid'][index] = list(set(pd_train['blog_uid'][index]))
+            pd_train['blog_id'][index].extend([i.split('\001')[0] for i in temp_list])
+            pd_train['blog_id'][index] = list(set(pd_train['blog_id'][index]))
             return temp_list  #确定只包含一个
         else:
             #print("未找到")
             return None #否则返回空
+
 
     def g(pd_data,con_column,target_column):
         if row[con_column] in list(pd_data[con_column]):  #如果pd中包含该uid，则找出对一个的value值，同时这个uid都是唯一的
@@ -117,6 +132,10 @@ def train_data(train_file=None):
         else:
             return None #否则返回空
 
+
+    """
+    对train数据进行merge
+    """
     print(pd_train)
     count = 0
     for index,row in pd_train.iterrows():
@@ -131,21 +150,50 @@ def train_data(train_file=None):
 
         if None not in list(row):
             count += 1
-
         #社交数据
         pd_train['follow'][index] = g(pd_follow,'uid','follow')
         pd_train['letter'][index] = g(pd_letter,'uid','letter')
-
-
     print("全部数据都有的有---"+str(count))
-
-
     print(pd_train)
 
-    if train_file:
-        pd_train.to_csv(train_file,encoding="utf8")
+    #将blog_id对应的uid找出
+    def h():
+        pass
+    pd_dev['blog_uid'] = [[]]*len(pd_dev)
+    pd_dev['blog_uid'] = pd_dev['blog_id'].apply(h)
 
-    return pd_train
+
+    """
+        对dev数据进行merge
+    """
+    print(pd_dev)
+    count = 0
+    for index, row in pd_dev.iterrows():
+        print("当前index为：" + str(index))
+        # 行为数据
+        pd_dev['post'][index] = f(pd_post, pd_dev, index, row, 'uid', 'post')
+        pd_dev['brows'][index] = f(pd_brows, pd_dev, index, row, 'uid', 'brows')
+        pd_dev['comment'][index] = f(pd_comment, pd_dev, index, row, 'uid', 'comment')
+        pd_dev['voteup'][index] = f(pd_voteup, pd_dev, index, row, 'uid', 'voteup')
+        pd_dev['votedown'][index] = f(pd_votedown, pd_dev, index, row, 'uid', 'votedown')
+        pd_dev['favorite'][index] = f(pd_favorite, pd_dev, index, row, 'uid', 'favorite')
+
+        if None not in list(row):
+            count += 1
+        # 社交数据
+        pd_dev['follow'][index] = g(pd_follow, 'uid', 'follow')
+        pd_dev['letter'][index] = g(pd_letter, 'uid', 'letter')
+    print("全部数据都有的有---" + str(count))
+    print(pd_dev)
+
+    if train_file:
+        pass
+        # pd_train.to_csv(train_file,encoding="utf8")
+
+    if dev_file:
+        pd_dev.to_csv(dev_file,encoding="utf8")
+
+    return (pd_train,pd_dev)
 
 def blog_info(filename):
     """
@@ -164,8 +212,9 @@ def blog_info(filename):
 
 
 if __name__ == '__main__':
-    # train_data(dp.TrainCsv)
-    blog_info(dp.TrainCsv)
+    train_and_dev_data(dp.TrainCsv,dp.DevCsv)
+
+    #blog_info(dp.TrainCsv)
 
 
 
