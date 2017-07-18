de3cce075fad526f3f516094996a6dedb90e3281
chauncyzhu <chauncyzhu@163.com>
2017-07-18 16:23:20 +0800

    keras实现神经网络来分类

---
+++ training.py
@@ -0,0 +1,96 @@
+# coding=utf8
+"""
+    使用神经网络来训练该数据
+"""
+import pandas as pd
+import numpy as np
+import utils.data_path as dp
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Embedding
+from keras.layers.recurrent import LSTM, GRU
+from keras.layers.core import Dropout,Dense,Activation
+from keras.utils import np_utils  #这个使用还不是很清楚
+
+"""
+    通过keras实现lstm神经网络，实际上只要确定好标签、输入、字典，就可以训练该神经网络
+"""
+
+def get_pickle_data(pd_pkl_name,columns_name):
+    """
+    产生embedding_matrix
+    :param pd_pkl_name: 需要加载的字典内容
+    :param columns_name: 需要保留的列
+    :return:
+    """
+    #从pd_pkl_name中读取文件
+    pd_pkl = pd.read_pickle(pd_pkl_name)
+    print(pd_pkl)
+
+    non_selected = list(set(pd_pkl.columns) - set(columns_name))
+    pd_pkl = pd_pkl.drop(non_selected, axis=1)   #去掉没有选择的列，实际上只保留id以及对应的vector
+
+    return pd_pkl
+
+
+def lstm(trainData,trainMark,testData,testMark,embedding_dim,embedding_matrix,maxlen):
+    # 填充数据，将每个序列长度保持一致
+    trainData = list(sequence.pad_sequences(trainData,maxlen=maxlen,dtype='float64'))  # sequence返回的是一个numpy数组，pad_sequences用于填充指定长度的序列，长则阶段，短则补0，由于下面序号为0时，对应值也为0，因此可以这样
+    testData = list(sequence.pad_sequences(testData,maxlen=maxlen,dtype='float64'))  # sequence返回的是一个numpy数组，pad_sequences用于填充指定长度的序列，长则阶段，短则补0
+
+    # 建立lstm神经网络模型
+    model = Sequential()  # 多个网络层的线性堆叠，可以通过传递一个layer的list来构造该模型，也可以通过.add()方法一个个的加上层
+    #model.add(Dense(256, input_shape=(train_total_vova_len,)))   #使用全连接的输入层
+    model.add(Embedding(len(embedding_matrix),embedding_dim,weights=[embedding_matrix],mask_zero=True,input_length=maxlen))  # 指定输入层，将高维的one-hot转成低维的embedding表示，第一个参数大或等于0的整数，输入数据最大下标+1，第二个参数大于0的整数，代表全连接嵌入的维度
+    # lstm层，也是比较核心的层
+    model.add(LSTM(128))  # 256对应Embedding输出维度，128是输入维度可以推导出来
+    model.add(Dropout(0.5))  # 每次在参数更新的时候以一定的几率断开层的链接，用于防止过拟合
+    model.add(Dense(1))  # 全连接，这里用于输出层，1代表输出层维度，128代表LSTM层维度可以自行推导出来
+    model.add(Activation('sigmoid'))  # 输出用sigmoid激活函数
+    # 编译该模型，binary_crossentropy（亦称作对数损失，logloss），adam是一种优化器，class_mode表示分类模式
+    model.compile(loss='binary_crossentropy', optimizer='adam', class_mode="binary")
+
+    # 正式运行该模型,我知道为什么了，因为没有补0！！每个array的长度是不一样的，因此才会报错
+    X = np.array(list(trainData))  # 输入数据
+    print("X:", X)
+    Y = np.array(list(trainMark))  # 标签
+    print("Y:", Y)
+    # batch_size：整数，指定进行梯度下降时每个batch包含的样本数
+    # nb_epoch：整数，训练的轮数，训练数据将会被遍历nb_epoch次
+    model.fit(X, Y, batch_size=16, nb_epoch=10)  # 该函数的X、Y应该是多个输入：numpy list(其中每个元素为numpy.array)，单个输入：numpy.array
+
+    # 进行预测
+    A = np.array(list(testData))  # 输入数据
+    print("A:", A)
+    B = np.array(list(testMark))  # 标签
+    print("B:", B)
+    classes = model.predict_classes(A)  # 这个是预测的数据
+    acc = np_utils.accuracy(classes, B)  # 计算准确率，使用还不是很清楚
+    print('Test accuracy:', acc)
+
+if __name__ == '__main__':
+    """
+    整理好标签数据
+    """
+    embedding = get_pickle_data(dp.SOME_BLOGCONTENT_VECTOR_NORMALIZE, columns_name=['blog_id','blog_jieba_vector'])
+
+    #获得训练数据和测试数据
+    pd_train = get_pickle_data(dp.TrainPKL, columns_name=['labels','uid','embedding_index'])
+    pd_test = get_pickle_data(dp.DevPKL, columns_name=['uid','embedding_index'])
+
+    #获得所有的标签
+    labels_name = pd.read_csv()
+    labels_len = 100
+    def f(x):
+        a = [0]*labels_len
+        x = x[0].split('\001')
+        for i in x:
+            a[labels_name.index(i)] = 1
+        return a
+
+    pd_train['labels'] = pd_train['labels'].apply(f)
+    print(pd_train)
+
+
+
+#    lstm(list(pd_train['sequence']), list(pd_train['class']), list(pd_test['sequence']), list(pd_test['class']), embedding_dim,embedding_matrix,maxlen)
